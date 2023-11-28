a1ef31ef73209ff977ed3034ac10a18d6c22e665
Hazel <ishazelpie@gmail.com>
2023-11-28 19:22:44 +0200

    Smol fix

--- index.ts
+++ index.ts
@@ -15,7 +15,6 @@ export async function fetchPenalCode() : Promise<string[]> {
 
     for (let i = 1; i < topics.length; i++) {
         topics[i] = topics[i].split('<div class="notice">')[0].split('<div class="back2top">')[0].replace(/(<([^>]+)>)/gi, '').replace(/&amp;/g, '').replace(/ +/g, ' ').split('\n').slice(16).join('\n') // Big mess, extracts out just the text from each topic for easier comparison.
-        Bun.write(`${i}`, topics[i])
     }
     return topics
 }
